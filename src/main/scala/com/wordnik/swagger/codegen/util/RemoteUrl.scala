6e22b45ae1b4f856c5d1a030bbe89e6caa6d0764
Kevin Rademan <kevin.rademan@hellofresh.de>
2015-01-28 17:42:10 +0000

    Added trustmodifier to ignore invalid ssl certificates

--- src/main/scala/com/wordnik/swagger/codegen/util/RemoteUrl.scala
+++ src/main/scala/com/wordnik/swagger/codegen/util/RemoteUrl.scala
@@ -11,22 +11,23 @@ trait RemoteUrl {
 	def urlToString(url: String, authorization: Option [AuthorizationValue]): String = {
 		var is: InputStream = null
 		try{
-			val conn: URLConnection = authorization match {
+			val conn: HttpURLConnection = authorization match {
 				case Some(auth: ApiKeyValue) => {
 					if(auth.passAs == "header") {
-						val connection = new URL(url).openConnection()
+						val connection = new URL(url).openConnection().asInstanceOf[HttpURLConnection]
 						connection.setRequestProperty(auth.keyName, auth.value)
 						connection
 					}
 					else if(auth.passAs == "query") {
-						new URL(url + "?%s=%s".format(URLEncoder.encode(auth.keyName, "UTF-8"), URLEncoder.encode(auth.value, "UTF-8"))).openConnection()
+						new URL(url + "?%s=%s".format(URLEncoder.encode(auth.keyName, "UTF-8"), URLEncoder.encode(auth.value, "UTF-8"))).openConnection().asInstanceOf[HttpURLConnection]
 					}
 					else {
-						new URL(url).openConnection()
+						new URL(url).openConnection().asInstanceOf[HttpURLConnection]
 					}
 				}
-				case None => new URL(url).openConnection()
+				case None => new URL(url).openConnection().asInstanceOf[HttpURLConnection]
 			}
+			TrustModifier.relaxHostChecking(conn)
 			is = conn.getInputStream()
 			Source.fromInputStream(is).mkString
 		}
