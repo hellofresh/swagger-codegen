ed874df7a37e3d3f6449755127893511e03f6264
Pepijn Senders <p.senders92@gmail.com>
2015-12-15 19:04:04 +0100

    Changes to client injection

--- modules/swagger-codegen/src/main/resources/hellofreshphp/Deserializer.php
+++ modules/swagger-codegen/src/main/resources/hellofreshphp/Deserializer.php
@@ -6,10 +6,68 @@ use GuzzleHttp\Message\ResponseInterface;
 
 class Deserializer
 {
+    /**
+     * @var array
+     */
+    protected static $types = [
+        'boolean',
+        'bool',
+        'integer',
+        'int',
+        'float',
+        'double',
+        'string',
+        'array',
+        'object',
+        'null'
+    ];
 
-    public static function deserialize($fqcn, ResponseInterface &$response)
+    /**
+     * @param  string            $fqcn
+     * @param  ResponseInterface $response
+     * @return \Illuminate\Support\Contracts\ArrayableInterface|null
+     */
+    public static function deserialize($fqcn, ResponseInterface $response)
     {
-        dd(func_get_args());
+        return static::recursiveAppointance($fqcn, json_decode($response->getBody()));
+    }
+
+    /**
+     * @param  string $className
+     * @param  stdClass $data
+     * @return \Illuminate\Support\Contracts\ArrayableInterface
+     */
+    protected static function recursiveAppointance($className, $data)
+    {
+        if (strpos($className, 'array[') === 0) {
+            $subClassName = substr($className, 6, -1);
+            $values = [];
+
+            foreach ($data as $value) {
+                $values[] = static::recursiveAppointance($subClassName, $value);
+            }
+
+            $data = $values;
+        } elseif ($className === 'DateTime') {
+            $data = new \DateTime($data);
+        } elseif (in_array($className, static::$types)) {
+            $value = (is_object($data) || is_array($data)) ? json_encode($data) : $data;
+            settype($value, $className);
+
+            $data = $value;
+        } elseif (class_exists($className)) {
+            $instance = new $className;
+
+            foreach ($className::$swaggerTypes as $property => $type) {
+                if (isset($data->$property)) {
+                    $instance->$property = static::recursiveAppointance($type, $data->$property);
+                }
+            }
+
+            $data = $instance;
+        }
+
+        return $data;
     }
 
 }
